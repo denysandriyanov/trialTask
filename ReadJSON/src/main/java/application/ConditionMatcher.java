10a65ee50a2b0ca723ff155820af35009bce642a
gldadnri <denys.andriyanov@avid.com>
2021-04-20 15:31:34 +0300

    removing redundant import

--- ReadJSON/src/main/java/application/ConditionMatcher.java
+++ ReadJSON/src/main/java/application/ConditionMatcher.java
@@ -2,7 +2,6 @@ package application;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
